ea95e5068c7cc501d87bb9affa7b9e43c0bbc09e
Blend Zebari <z.blend1@gmail.com>
2023-08-05 14:12:45 +0200

    ändringar

--- Portfolio/Projekt/Miniraknare/Construct.js
+++ Portfolio/Projekt/Miniraknare/Construct.js
@@ -1,9 +1,5 @@
 "use strict";
 
-let currentnumber = "";
-let previousnumber = "";
-let operator = "";
-let clearOnNextNum = false;
 const numberButtons = document.querySelectorAll(".num-btn");
 const operatorButtons = document.querySelectorAll(".operator");
 const equal = document.querySelector(".equal");
@@ -12,6 +8,7 @@ const clearBtn = document.querySelector(".clear"); //clear everything
 const clearEntryBtn = document.querySelector(".clear_entry"); //clear currententry
 const deleteBtn = document.querySelector(".delete"); //delete single value in currententry
 const plusminusBtn = document.querySelector(".plusminus"); //turn numbervalue positive or negative
-const previousDisplay = document.querySelector("display_previousentry");
+const previousDisplay = document.querySelector(".display_previousentry");
 const currentDisplay = document.querySelector(".display_currententry");
+
 
